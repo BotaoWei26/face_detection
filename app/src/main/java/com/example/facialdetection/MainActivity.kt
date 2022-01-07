ac2d908b334c339feaeb3540418405e82e8f6256
BotaoWei26 <58021376+botaowei26@users.noreply.github.com>
2022-01-07 01:50:00 -0500

    added camera

--- app/src/main/java/com/example/facialdetection/MainActivity.kt
+++ app/src/main/java/com/example/facialdetection/MainActivity.kt
@@ -2,10 +2,66 @@ package com.example.facialdetection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.facialdetection.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraSelector: CameraSelector
+    private val cameraProviderResult  =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
+            if (permissionGranted) {
+                startCamera()
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "The camera permission is necessary",
+                    Snackbar.LENGTH_INDEFINITE
+                ).show()
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+
+    }
+    private fun startCamera(){
+        // listening for data from the camera
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // connecting a preview use case to the preview in the xml file.
+            val preview = Preview.Builder().build().also{
+                it.setSurfaceProvider(binding.preview.surfaceProvider)
+            }
+            try{
+                // clear all the previous use cases first.
+                cameraProvider.unbindAll()
+                // binding the lifecycle of the camera to the lifecycle of the application.
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+            } catch (e: Exception) {
+                Log.d(TAG, "Use case binding failed")
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    companion object {
+        val TAG = "MainActivity"
     }
 }
