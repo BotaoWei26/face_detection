5c4462f7c5438c172e6dac28b0c3f3cb0fd9b40c
BotaoWei26 <58021376+botaowei26@users.noreply.github.com>
2022-01-07 02:51:28 -0500

    Added capture and gallery buttons

--- app/src/main/java/com/example/facialdetection/MainActivity.kt
+++ app/src/main/java/com/example/facialdetection/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.example.facialdetection
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.example.facialdetection.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraSelector: CameraSelector
-    private val cameraProviderResult  =
+    private var imageCapture: ImageCapture? = null
+    private lateinit var imgCaptureExecutor: ExecutorService
+    private val cameraPermissionResult =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
             if (permissionGranted) {
                 startCamera()
@@ -29,7 +44,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,28 +51,85 @@ class MainActivity : AppCompatActivity() {
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+
+        imgCaptureExecutor = Executors.newSingleThreadExecutor()
+
+        cameraPermissionResult.launch(android.Manifest.permission.CAMERA)
+
+        binding.imgCaptureBtn.setOnClickListener {
+            takePhoto()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                animateFlash()
+            }
+        }
+
+        binding.switchBtn.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                CameraSelector.DEFAULT_FRONT_CAMERA
+            } else {
+                CameraSelector.DEFAULT_BACK_CAMERA
+            }
+            startCamera()
+        }
+        binding.galleryBtn.setOnClickListener {
+            val intent = Intent(this, GalleryActivity::class.java)
+            startActivity(intent)
+        }
 
     }
-    private fun startCamera(){
-        // listening for data from the camera
+
+    private fun startCamera() {
+        val preview = Preview.Builder().build().also {
+            it.setSurfaceProvider(binding.preview.surfaceProvider)
+        }
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
-            // connecting a preview use case to the preview in the xml file.
-            val preview = Preview.Builder().build().also{
-                it.setSurfaceProvider(binding.preview.surfaceProvider)
-            }
-            try{
-                // clear all the previous use cases first.
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
                 cameraProvider.unbindAll()
-                // binding the lifecycle of the camera to the lifecycle of the application.
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
             } catch (e: Exception) {
                 Log.d(TAG, "Use case binding failed")
             }
-
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        imageCapture?.let {
+            val fileName = "JPEG_${System.currentTimeMillis()}"
+            val file = File(externalMediaDirs[0], fileName)
+            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+            it.takePicture(
+                outputFileOptions,
+                imgCaptureExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        Log.i(TAG, "The image has been saved in ${file.toUri()}")
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        Toast.makeText(
+                            binding.root.context,
+                            "Error taking photo",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.d(TAG, "Error taking photo:$exception")
+                    }
+
+                })
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun animateFlash() {
+        binding.root.postDelayed({
+            binding.root.foreground = ColorDrawable(Color.WHITE)
+            binding.root.postDelayed({
+                binding.root.foreground = null
+            }, 50)
+        }, 100)
     }
 
     companion object {
